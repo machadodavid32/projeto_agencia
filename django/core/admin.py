afbc18feaae22037ecd10bcc90ac048da508a2c6
David <machadodavid32@gmail.com>
2022-10-06 19:51:36 -0300

    Adicionando a parte adminstrativa ao site e incluindo projetos de teste exemplo

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Projetos
+
+
+
+class ProjetosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'desc')
+
+
+admin.site.register(Projetos, ProjetosAdmin)
--- django/core/models.py
+++ django/core/models.py
@@ -1,3 +1,15 @@
+from inspect import ClassFoundException
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Projetos(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    desc = models.CharField('descricao', max_length=500)
+
+    def __str__(self) -> str:   # Esta função serve para que o nome do projeto apareça na tela de projetos do adm do site
+        return self.nome
+    
+
+
+
+
